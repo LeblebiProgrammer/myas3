77eb83dc0ebbcc01882b0a3428b11b7dd82afe46
Sertac S. Gorkem <ssg103@cpp.cs.rutgers.edu>
2019-05-06 15:58:56 -0400

    sha

--- client.c
+++ client.c
@@ -488,10 +488,27 @@ int hash(char *str) {
 
 int main(int argc, char **argv)
 {
-  char *str = "Hello Mikey hwo are eyour\n\t";
-  size_t length = strlen(data);
-  unsigned char hash[SHA_DIGEST_LENGTH];
-  SHA1(data, length, hash);
+  unsigned char temp[SHA_DIGEST_LENGTH];
+    char buf[SHA_DIGEST_LENGTH*2];
+ 
+    if ( argc != 2 ) {
+        printf("Usage: %s string\n", argv[0]);
+        return -1;
+    }
+ 
+    memset(buf, 0x0, SHA_DIGEST_LENGTH*2);
+    memset(temp, 0x0, SHA_DIGEST_LENGTH);
+ 
+    SHA1((unsigned char *)argv[1], strlen(argv[1]), temp);
+ 
+    for (i=0; i < SHA_DIGEST_LENGTH; i++) {
+        sprintf((char*)&(buf[i*2]), "%02x", temp[i]);
+    }
+ 
+    printf("SHA1 of %s is %s\n", argv[1], buf);
+ 
+    return 0;
+
   // int k = hash(str);
   // printf("%d",k);
     // if(argc > 1){
